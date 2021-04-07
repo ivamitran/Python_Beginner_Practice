c9e44772558547310a5ca359b1a6a9d46e5dafb9
Ivan Tran <ivamitran16432@gmail.com>
2021-04-06 20:59:36 -0700

    Added API to findSmallest and findLargest procedures

--- practice.py
+++ practice.py
@@ -3,7 +3,9 @@ list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 list2 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 list3 = [1, 2, 100, 10, 2, 4, 8, 10]
 
-
+# findSmallest finds the smallest number in a list
+# list {list} - the list that you want to find the smallest number from
+# return {number} - returns the smallest number in the list you passed in for the argument
 def findSmallest(list):
     min = list[0]
     for number in list:
@@ -17,7 +19,9 @@ print(findSmallest(list1))
 print(findSmallest(list2))
 print(findSmallest(list3))
 
-
+# findLargest finds the largest number in a list
+# list {list} - the list that you want to find the largest number from
+# return {number} - returns the largest number in the list
 def findLargest(list):
     max = list[0]
     for number in list:
